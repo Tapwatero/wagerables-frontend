88b92c59e77bb5b51f08a567b89e31d83fd0b3b3
Tapwatero <charliewoolf20@gmail.com>
2023-04-25 21:42:21 +0100

    better mobile Leaderboard.tsx design

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -33,13 +33,13 @@ function Leaderboard(): JSX.Element {
 
 
     const list = leaderboard.map((leaderboardPosition: string[], index: number) =>
-        <ul className={"opacity-95 hover:bg-indigo-400 p-2 select-none hover:text-white duration-200 text-white text-center w-full text-center text-2xl"} key={index}>{index + 1}. {leaderboardPosition[0]} ({leaderboardPosition[1]})</ul>
+        <ul className={"opacity-95 hover:bg-indigo-400 p-2 select-none hover:text-white duration-200 text-white text-center w-full text-center text-md md:text-2xl"} key={index}>{index + 1}. {leaderboardPosition[0]} ({leaderboardPosition[1]})</ul>
     );
 
 
     return (
             <div className={"select-none flex justify-center items-center h-screen min-h-fit w-screen pattern-rhombus pattern-sky-50 pattern-bg-indigo-400 pattern-size-8 pattern-opacity-40"}>
-                <div className={"py-0.5 scrollbar-hide overflow-y-scroll border-4 border-violet-200 flex-col bg-indigo-500 h-4/6 w-11/12 md:w-3/5 xl:w-2/5 rounded-2xl opacity-95"}>
+                <div className={"py-0.5 scrollbar-hide overflow-y-scroll border-4 border-violet-200 flex-col bg-indigo-500 h-5/6 md:h-4/6 w-11/12 md:w-3/5 xl:w-2/5 rounded-2xl opacity-95"}>
                     <LeaderboardHeader totalVotes={totalVotes}></LeaderboardHeader>
                     <div className={"w-full grid grid-cols-1 gap-0 divide-y-2 border-y-2"}>
                         {list}
--- src/components/LeaderboardHeader.tsx
+++ src/components/LeaderboardHeader.tsx
@@ -8,7 +8,7 @@ interface LeaderboardHeaderProps {
 function LeaderboardHeader(props: LeaderboardHeaderProps): JSX.Element {
     return (
         <div>
-            <h1 className={"text-2xl text-white text-center p-4 font-medium"}>Leaderboard - ({props.totalVotes} Votes)</h1>
+            <h1 className={"text-xl md:text-2xl text-white text-center p-4 font-medium"}>Leaderboard - ({props.totalVotes} Votes)</h1>
         </div>
     )
 }
