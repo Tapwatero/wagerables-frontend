74367bee2a996fbcadd8594aedbf7cb44851cc71
Tapwatero <70451052+Tapwatero@users.noreply.github.com>
2023-04-27 14:22:36 +0100

    Update VoteHeader.tsx

--- src/components/VoteHeader.tsx
+++ src/components/VoteHeader.tsx
@@ -8,7 +8,7 @@ interface VoteHeaderProps {
 function VoteHeader(props: VoteHeaderProps): JSX.Element {
     return (
         <div className={"bg-indigo-300 full p-4 flex items-center justify-center flex-col"}>
-            <h1 className={"text-center text-white text-2xl"}>Who's the bigger npc?</h1>
+            <h1 className={"text-center text-white text-2xl"}>Who's the bigger goofball?</h1>
             <div className={"flex flex-row gap-x-4"}>
                 <Link to={"/leaderboard"}><h1 className={"select-none underline duration-300 hover:text-yellow-300 text-white text-2xl"}>View Leaderboard</h1>
                 </Link>
